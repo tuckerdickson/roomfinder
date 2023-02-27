646055dd8c4d44f4fdb04e77e1bce4d1a6fd7863
Tucker Dickson <nathan-dickson@uiowa.edu>
2023-02-27 08:32:33 -0600

    commented Styles.swift

--- roomfindr/Styles.swift
+++ roomfindr/Styles.swift
@@ -1,24 +1,36 @@
 /*
-See LICENSE folder for this sample’s licensing information.
+ 
+ Styles.swift
+ roomfindr
 
-Abstract:
-Extensions to the IMDF types to enable styling overlays/annotations based on their properties.
-*/
+ This file defines the styles for all annotations and overlays.
+ Side notes:
+    - the various UIColor object referenced in this file are defined in the assets folder.
+    - strokeColor defines the color of lines
+    - fillColor defines the color of an area
+    - backgroundColor defines the color of annotations (circular markers)
+
+ Created on 2/25/23.
+ 
+ */
 
 import MapKit
 
+// a protocol defines an outline for methods that can be adopted by classes
+// this protocol defines a template for features which contain geometry, annotations, and overlays
 protocol StylableFeature {
     var geometry: [MKShape & MKGeoJSONObject] { get }
     func configure(overlayRenderer: MKOverlayPathRenderer)
     func configure(annotationView: MKAnnotationView)
 }
 
-// Provide default empty implementations for these protocol methods.
+// default implementations for StylableFeature protocol methods.
 extension StylableFeature {
     func configure(overlayRenderer: MKOverlayPathRenderer) {}
     func configure(annotationView: MKAnnotationView) {}
 }
 
+// defines the color and width of level lines
 extension Level: StylableFeature {
     func configure(overlayRenderer: MKOverlayPathRenderer) {
         overlayRenderer.strokeColor = UIColor(named: "LevelStroke")
@@ -26,31 +38,29 @@ extension Level: StylableFeature {
     }
 }
 
+// defines color and linewidth for units based on their category
 extension Unit: StylableFeature {
-    // A list of unit categories which we are interested in styling in unique ways. Note: This is not a full list of possible IMDF category names.
+    // the different types of units that we use
     private enum StylableCategory: String {
         case elevator
-        case escalator
         case stairs
         case restroom
         case restroomMale = "restroom.male"
         case restroomFemale = "restroom.female"
         case room
-        case nonpublic
         case walkway
     }
     
+    // specify the approprite fill color for the unit based on its category
     func configure(overlayRenderer: MKOverlayPathRenderer) {
         if let category = StylableCategory(rawValue: self.properties.category) {
             switch category {
-            case .elevator, .escalator, .stairs:
+            case .elevator, .stairs:
                 overlayRenderer.fillColor = UIColor(named: "ElevatorFill")
             case .restroom, .restroomMale, .restroomFemale:
                 overlayRenderer.fillColor = UIColor(named: "RestroomFill")
             case .room:
                 overlayRenderer.fillColor = UIColor(named: "RoomFill")
-            case .nonpublic:
-                overlayRenderer.fillColor = UIColor(named: "NonPublicFill")
             case .walkway:
                 overlayRenderer.fillColor = UIColor(named: "WalkwayFill")
             }
@@ -58,24 +68,31 @@ extension Unit: StylableFeature {
             overlayRenderer.fillColor = UIColor(named: "DefaultUnitFill")
         }
 
+        // line color and width is the same for all units
         overlayRenderer.strokeColor = UIColor(named: "UnitStroke")
         overlayRenderer.lineWidth = 1.3
     }
 }
 
+// defines the color and width of opening lines
 extension Opening: StylableFeature {
     func configure(overlayRenderer: MKOverlayPathRenderer) {
-        // Match the standard unit fill color so the opening lines match the open areas' colors.
+        // set the line color of openings to match the walkway fill color
+        // makes the opening lines "invisible"
         overlayRenderer.strokeColor = UIColor(named: "WalkwayFill")
         overlayRenderer.lineWidth = 2.0
     }
 }
 
+// defines color for differenty types of amenities
 extension Amenity: StylableFeature {
+    // the different types of amenities that we're insterested in
+    // TODO: figure out if we have any cases to add here; if not, delete this
     private enum StylableCategory: String {
         case exhibit
     }
     
+    // define annotation colors for different categories of amenities
     func configure(annotationView: MKAnnotationView) {
         if let category = StylableCategory(rawValue: self.properties.category) {
             switch category {
@@ -86,17 +103,20 @@ extension Amenity: StylableFeature {
             annotationView.backgroundColor = UIColor(named: "DefaultAmenityFill")
         }
         
-        // Most Amenities have lower display priority then Occupants.
+        // most amenities have lower display priority than occupants
         annotationView.displayPriority = .defaultLow
     }
 }
 
+// defines the annotation color for different types of occupants
 extension Occupant: StylableFeature {
+    // the types of occupant that we're interested in
     private enum StylableCategory: String {
         case restaurant
         case shopping
     }
 
+    // configure the annotation color for each type of occupant above
     func configure(annotationView: MKAnnotationView) {
         if let category = StylableCategory(rawValue: self.properties.category) {
             switch category {
@@ -107,6 +127,7 @@ extension Occupant: StylableFeature {
             }
         }
 
+        // typically, occupants have higher priority than amenities
         annotationView.displayPriority = .defaultHigh
     }
 }
