b03ce421e2eac90e2b99ae71db73fbd8e7e83739
Cathryn Lyons <cathryn-lyons@uiowa.edu>
2023-04-19 18:11:16 -0500

    Working on path queue

--- roomfinder/EdgeManager.swift
+++ roomfinder/EdgeManager.swift
@@ -9,8 +9,14 @@
 import Foundation
 import MapKit
 
-
 class EdgeManager {
+    
+    //list of stairs on all 3 levels
+    let stairList3 = [["1259", "2259", "3259"], ["1300", "2300", "3300"], ["1203", "2203", "3203"], ["1100", "2100", "3100"], ["151", "251", "335"], ["1233", "2233", "3233"]]
+    //list of stairs on only floors 1 and 2
+    let stairList2 = [["1126", "2126"], ["1400", "2400"]]
+    
+    var stairPoints : [Simple2DNode] = []
     
     struct Geodata: Codable {
         let type: String
@@ -51,6 +57,34 @@ class EdgeManager {
            print("\(error)")
         }
         
+        //make connections for stairList3
+        for stairs in stairList3{
+            let index1 = nodes.0.firstIndex(of: stairs[0])!
+            let index2 = nodes.0.firstIndex(of: stairs[1])!
+            let index3 = nodes.0.firstIndex(of: stairs[2])!
+            
+            stairPoints.append(nodes.1[index1])
+            stairPoints.append(nodes.1[index2])
+            stairPoints.append(nodes.1[index3])
+            
+            createConnection(from: nodes.1[index1], to: nodes.1[index2])
+            createConnection(from: nodes.1[index2], to: nodes.1[index3])
+            createConnection(from: nodes.1[index3], to: nodes.1[index2])
+            createConnection(from: nodes.1[index2], to: nodes.1[index1])
+        }
+        //make connections for stairList2
+        for stairs in stairList2{
+            let index1 = nodes.0.firstIndex(of: stairs[0])!
+            let index2 = nodes.0.firstIndex(of: stairs[1])!
+            
+            stairPoints.append(nodes.1[index1])
+            stairPoints.append(nodes.1[index2])
+            
+            createConnection(from: nodes.1[index1], to: nodes.1[index2])
+            createConnection(from: nodes.1[index2], to: nodes.1[index1])
+        }
+    
+        
         for edge in edgeData!.features{
             //print(edge.properties.nodes)
             let index1 = nodes.0.firstIndex(of: String(edge.properties.nodes[0]))!
@@ -63,7 +97,8 @@ class EdgeManager {
     }
     func pathFind(to start: Simple2DNode, from end: Simple2DNode) -> [Simple2DNode]{
         let path = start.findPath(to: end)
-        print(type(of: path))
+
+        
         return path
     }
 }
--- roomfinder/IndoorMapViewController.swift
+++ roomfinder/IndoorMapViewController.swift
@@ -11,7 +11,8 @@ import CodeScanner
 import SwiftUI
 
 class IndoorMapViewController: UIViewController, UISearchBarDelegate, LevelPickerDelegate {
-    
+    let manager = EdgeManager()
+    var pathQueue : [[Simple2DNode]] = []
     // storyboard elements
     @IBOutlet var mapView: MKMapView!
     @IBOutlet weak var searchBar: UISearchBar!
@@ -159,7 +160,8 @@ class IndoorMapViewController: UIViewController, UISearchBarDelegate, LevelPicke
         self.view.addGestureRecognizer(tap)
         
         // construct the graph of units
-        EdgeManager().parse(nodes: nodes)
+        
+        manager.parse(nodes: nodes)
     }
     
     /// Displays the features for the current ordinal.
@@ -226,18 +228,48 @@ class IndoorMapViewController: UIViewController, UISearchBarDelegate, LevelPicke
         // select the appropriate level and update the features to reflect it
         let selectedLevel = self.levels[selectedIndex]
         showFeaturesForOrdinal(selectedLevel.properties.ordinal)
+        
     }
+    
     
     func getPath(toRoom: String){
         var path: [Simple2DNode] = []
+
         let toIndex = nodes.0.firstIndex(of: toRoom)!
         let fromIndex = nodes.0.firstIndex(of: fromRoom)!
         
-        path = EdgeManager().pathFind(to: nodes.1[toIndex], from: nodes.1[fromIndex])
-        print(path)
+        path = manager.pathFind(to: nodes.1[toIndex], from: nodes.1[fromIndex])
+        
+        //figure out if or where you need to break the path
+        var breakIndex : [Int] = []
+        var i : Int = 0
+        for element in path {
+            //check to see if the point in the path is a stair point
+            if manager.stairPoints.contains(element) {
+                breakIndex.append(i)
+            }
+            i += 1
+        }
+        
+        //if the path contained a stair element
+        if breakIndex != []{
+            var prevBreak : Int = 0
+            for breaks in breakIndex{
+                if prevBreak == 0{
+                    pathQueue.append(Array(path[prevBreak ..< breaks+1]))
+                }else{
+                    pathQueue.append(Array(path[prevBreak-1 ..< breaks+1]))
+                }
+                
+                prevBreak = breaks
+            }
+        }else{
+            pathQueue.append(path)
+        }
+        
         
         var coordinates: [CLLocationCoordinate2D] = []
-        for node in path{
+        for node in pathQueue[0] {
             let roomIndex = nodes.1.firstIndex(of: node)!
             print(node.position)
             print(nodes.0[roomIndex])
@@ -246,11 +278,22 @@ class IndoorMapViewController: UIViewController, UISearchBarDelegate, LevelPicke
             let long = CLLocationDegrees(node.position.x)
             coordinates.append(CLLocationCoordinate2D(latitude: lat, longitude: long))
         }
+        //remove the path that is first displayed from pathQueue
+        let index = nodes.1.firstIndex(of: pathQueue[0][0])!
+        let floor = nodes.0[index]
+        pathQueue.remove(at: 0)
         
+        if pathQueue != [] {
+            //change the floor level so it correlates with first part of path
+            selectedLevelDidChange(selectedIndex: floorOptions.count - floor.first!.wholeNumberValue!)
+            levelPicker.selectedIndex = floorOptions.count - floor.first!.wholeNumberValue!
+        }
         mapView.removeOverlay(currentPathOverlay)
         currentPathOverlay = MKPolyline(coordinates: &coordinates, count: coordinates.count)
         currentPathOverlay.title = "Path"
         mapView.addOverlay(currentPathOverlay)
+        
+        
     }
     
     func filterRooms(searchTerm: String) {
